794b1a5c71fbf391c09844a0029e2e37d68ae76d
Daniel Bauman <danielbauman1010@gmail.com>
2020-08-03 21:01:59 -0700

    cleaned react setup

---
+++ front/todo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>TODO</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
